bac29025db587480e2bf4238fc890a5683bc9bc6
Kenneth Hedman <github@vitberget.se>
2021-02-23 19:12:15 +0100

    Day 22 part 2 - logTime -> measuredTimedValue

--- app/src/main/kotlin/se/vbgt/aoc/year2020/day22/RunDay22.kt
+++ app/src/main/kotlin/se/vbgt/aoc/year2020/day22/RunDay22.kt
@@ -4,21 +4,17 @@ import se.vbgt.aoc.year2020.day22.common.fileToCrabCombatState
 import se.vbgt.aoc.year2020.day22.part1.part1
 import se.vbgt.aoc.year2020.day22.part2.part2
 import kotlin.system.measureTimeMillis
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+import kotlin.time.measureTimedValue
 
+@ExperimentalTime
 fun main() {
     val puzzleCombatState = fileToCrabCombatState("puzzle22.txt")
 
-    val (p1, t1) = logTime { part1(puzzleCombatState)}
-    println("Part1: ${p1}, took ${t1}ms")
+    val (p1,t1) = measureTimedValue {part1(puzzleCombatState)}
+    println("Part1: ${p1}, took ${t1}")
 
-    val (p2, t2) = logTime { part2(puzzleCombatState) }
-    println("Part2: ${p2}, took ${t2}ms")
-}
-
-fun logTime(function: () -> Any): Pair<Any,Long> {
-    var a : Any
-    val t = measureTimeMillis {
-        a = function()
-    }
-    return a to t
+    val (p2, t2) = measureTimedValue { part2(puzzleCombatState) }
+    println("Part2: ${p2}, took ${t2}")
 }
