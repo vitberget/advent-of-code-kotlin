be4334aa34420091668e350ba5c24d31c42f2ee4
Kenneth Hedman <github@vitberget.se>
2021-03-02 21:04:36 +0100

    Day 23, comments

--- app/src/main/kotlin/se/vbgt/aoc/year2020/day17/part1/Part1.kt
+++ app/src/main/kotlin/se/vbgt/aoc/year2020/day17/part1/Part1.kt
@@ -2,7 +2,14 @@ package se.vbgt.aoc.year2020.day17.part1
 
 import java.io.File
 
-data class Position(val x: Int, val y: Int, val z: Int)
+data class Position(
+    val x: Int,
+    val y: Int,
+    val z: Int
+)
+/**
+ * Sets are awesome!
+ */
 typealias Positions = Set<Position>
 
 fun part1(filename: String): Int {
@@ -15,17 +22,36 @@ fun part1(filename: String): Int {
     return state.size
 }
 
+/**
+ * Play a game of Conway in 3d
+ *
+ * First take all existing cells and use the cells surrounding theese
+ *
+ * Then filter all the possible cells, and only keep the ones that match
+ * the rules that keeps or turns a cell active
+ */
 fun playConway3d(positions: Positions): Positions {
     val positionsToTest = positions.map {
         surroundingPositions(it)
     }.flatten()
 
     return positionsToTest.filter {
-        val living = surroundingPositions(it).intersect(positions).size
-        living == 3 || living == 2 && positions.contains(it)
+        // Intersect = all item that exists in both sets, which gives us all the
+        // surrounding cells of a positions that exists in the starting game positions
+        // then count them
+        val noOfActiveNeighbourCells = surroundingPositions(it).intersect(positions).size
+        // the rules from the game, with both dictates that a cell will be active
+        // if it has three active cells. Left is that an active cell keeps it status
+        // if it has two active cells as neighbours.
+        noOfActiveNeighbourCells == 3 || noOfActiveNeighbourCells == 2 && positions.contains(it)
     }.toSet()
 }
 
+
+/**
+ * Delta positions surrounding a position. Filter out where all coordinates
+ * are zero (this is the position itself
+ */
 val surroundingDeltas = generateSurroundingDeltas()
 fun generateSurroundingDeltas(): List<Position> {
     val mutList = mutableListOf<Position>()
@@ -39,6 +65,10 @@ fun generateSurroundingDeltas(): List<Position> {
     return mutList.toList()
 }
 
+/**
+ * Take the set of surrounding deltas and map them all on the
+ * positions, giving all the cells surrounding that position
+ */
 fun surroundingPositions(position: Position): Positions =
     surroundingDeltas.map {
         Position(
@@ -48,6 +78,10 @@ fun surroundingPositions(position: Position): Positions =
         )
     }.toSet()
 
+/**
+ * Read characters #, and give them position x,y and z. (z is 0)
+ * Put them all in a set of positions aka Positions
+ */
 fun readFileToState(filename: String): Positions =
     File(filename).readLines()
         .mapIndexed { y, line ->
--- app/src/main/kotlin/se/vbgt/aoc/year2020/day17/part2/Part2.kt
+++ app/src/main/kotlin/se/vbgt/aoc/year2020/day17/part2/Part2.kt
@@ -2,27 +2,36 @@ package se.vbgt.aoc.year2020.day17.part2
 
 import java.io.File
 
-data class Position(val x: Int, val y: Int, val z: Int, val w: Int)
+data class Position(
+    val x: Int,
+    val y: Int,
+    val z: Int,
+    val w: Int
+)
 typealias Positions = Set<Position>
 
+/**
+ * Nothing really more special for my 4d solutions over my 3d solution,
+ * except that there is now a fourth dimension of course.
+ */
 fun part2(filename: String): Int {
     var state = readFileToState(filename)
 
     for (game in 1..6) {
-        state = playConway3d(state)
+        state = playConway4d(state)
     }
 
     return state.size
 }
 
-fun playConway3d(positions: Positions): Positions {
+fun playConway4d(positions: Positions): Positions {
     val positionsToTest = positions.map {
         surroundingPositions(it)
     }.flatten()
 
     return positionsToTest.filter {
-        val living = surroundingPositions(it).intersect(positions).size
-        living == 3 || living == 2 && positions.contains(it)
+        val noOfActiveNeighbourCells = surroundingPositions(it).intersect(positions).size
+        noOfActiveNeighbourCells == 3 || noOfActiveNeighbourCells == 2 && positions.contains(it)
     }.toSet()
 }
 
