e0e8181326616ea0113f54f842c8481975b98353
Kenneth Hedman <github@vitberget.se>
2021-04-01 22:11:02 +0200

    Day 5, cleaning

--- app/src/main/kotlin/se/vbgt/aoc/year2020/day5/Range.kt
+++ app/src/main/kotlin/se/vbgt/aoc/year2020/day5/Range.kt
@@ -2,8 +2,13 @@ package se.vbgt.aoc.year2020.day5
 
 data class Range(
     val lower: Int,
-    val higher: Int
+    val higher: Int,
 ) {
-    fun lowerHalf(): Range = this.copy(higher = this.lower + ((this.higher - this.lower) / 2))
-    fun upperHalf(): Range = this.copy(lower = this.lower + ((this.higher - this.lower) / 2) + 1)
+    fun lowerHalf(): Range = copy(
+        higher = lower + (higher - lower) / 2
+    )
+
+    fun upperHalf(): Range = copy(
+        lower = lower + (higher - lower) / 2 + 1
+    )
 }
