af2d907bc1efceb838ef7f46de3daa4a49c61e23
Kenneth Hedman <github@vitberget.se>
2022-10-27 13:54:41 +0200

    nicer

--- app/src/main/kotlin/se/vbgt/aoc/year2021/day1/Day1.kt
+++ app/src/main/kotlin/se/vbgt/aoc/year2021/day1/Day1.kt
@@ -2,15 +2,16 @@ package se.vbgt.aoc.year2021.day1
 
 import java.io.File
 
-private fun zippie(delta: Int, numbers: List<Int>): Int =
-        numbers.zip(numbers.drop(delta))
-                .count { (a, b) -> a < b }
+private fun zipSolver(delta: Int, numbers: List<Int>): Int =
+    numbers.drop(delta)
+        .zip(numbers)
+        .count { (a, b) -> a > b }
 
 fun main() {
-    val nums = File("puzzle-2021-01.txt")
-            .readLines()
-            .map { it.toInt() }
+    val depthNumbers = File("puzzle-2021-01.txt")
+        .readLines()
+        .map { it.toInt() }
 
-    println("part1 ${zippie(1, nums)}") // 1387
-    println("part2 ${zippie(3, nums)}") // 1362
+    println("part1 ${zipSolver(1, depthNumbers)}") // 1387
+    println("part2 ${zipSolver(3, depthNumbers)}") // 1362
 }
